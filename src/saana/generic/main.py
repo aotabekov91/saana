d50730dc88df9be8c25657a1b880920b497cf486
adam <adam@myworld.net>
2023-10-16 02:12:14 +0200

    minor

--- src/saana/generic/main.py
+++ src/saana/generic/main.py
@@ -8,28 +8,50 @@ class Generic(Plug):
         self.functions={
                 'setApp': self.setApp,
                 'setMode': self.setMode,
+                'showState': self.showState,
                 }
+
+    def getApps(self, mode):
+
+        self.umay=self.app.moder.plugs.umay
+        res=self.umay.send({'getKeywords':{}})
+        res=res.get('getKeywords', {})
+        k=res.get('keywords', {})
+        matches=[]
+        for app, data in k.items():
+            for m in data.get('mode', []):
+                if mode in m:
+                    matches+=[app]
+        return matches
+
+    def showState(self):
+
+        self.umay=self.app.moder.plugs.umay
+        res=self.umay.send({'getState':{}})
+        print(res)
 
     def setMode(self, mode=None):
 
         self.umay=self.app.moder.plugs.umay
         res=self.umay.send({'getState':{}})
-        prev_app=res['getState'].get('prev_app')
-
-        if prev_app!=self.app.name:
+        prev=res['getState'].get('prev_app')
+        if prev==self.app.name or not prev:
+            super().setMode(mode)
+        else:
             action={'Generic_setMode':{'mode':mode}}
             data = {
-                'app':prev_app, 
+                'app':prev, 
                 'action':action
                }
             res = self.umay.send(
                 {'act':data})
             print(res)
-        else:
-            super().setMode(mode)
-        data = {'mode':mode}
+        apps=self.getApps(mode)
+        if len(apps)==1:
+            self.setApp(apps[0])
         res = self.umay.send(
-            {'setMode':data})
+                {'setMode': {'mode':mode}}
+             )
         print(res)
 
     def setApp(self, app=None):
@@ -43,19 +65,17 @@ class Generic(Plug):
     def handleRequest(self, request):
 
         self.umay=self.app.moder.plugs.umay
-        res=self.umay.send(
-                {'getState':{}})
+        res=self.umay.send({'getState':{}})
         prev=res['getState'].get('prev_app')
-        if prev!=self.app.name:
+        if prev==self.app.name or not prev:
+            self.runPlugAction(request)
+        else:
             data = {
                     'app':prev, 
                     'action':request
                    }
             res = self.umay.send({'act':data})
             print(f'Saana rerouting to {prev}:', res)
-        else:
-            self.runPlugAction(request)
-
 
     def runPlugAction(self, request):
 
